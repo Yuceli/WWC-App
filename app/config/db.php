6806a1752aa2496eca7274730e3f29e58e1ceace
Arandi Lopez <arandilopez.93@gmail.com>
2015-01-22 22:25:05 -0600

    añadir un bundle para el codigo

---
+++ app/config/db.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+*	Configuracion de la conexion de la base de datos
+*/
+define('DB_URL', '');
+define('DB_USER', 'value');
+define('DB_PASSWORD', 'value');
+define('DB_PORT', 'value');
+define('DB_NAME', 'value');
---
+++ app/config/migration.sql
@@ -0,0 +1,9 @@
+--- Favor de llenar con los datos de los movimientos
+--- que hagan en su base de datos.
+--- Esto es para poder tener el mismo esqueme conforme se va desarrollando
+--- Este archivo se deberia llenar bloque por bloque con la siguiente informacion:
+--- Autor:
+--- Fecha de push
+--- EL bloque de codigo SQL que hace el cambio en la base de datos
+--- Breve descripcion de que carajos hace...
+--- Gracias
