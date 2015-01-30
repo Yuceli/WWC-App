81e78e5cc774fbafe46fd6497b8ea5fe41ddc701
Arandi Lopez <arandilopez.93@gmail.com>
2015-01-29 21:14:06 -0600

    fix de footer

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,7 +1,11 @@
 
 		
+
 	</div>
 </div> <! Final del div main>
+<footer>
+	<p>Copyright &copy; 2015 - WWC Mérida</p>
+</footer>
 <script type="text/javascript" src="scripts/app.js"></script>
 </body>
 </html>
