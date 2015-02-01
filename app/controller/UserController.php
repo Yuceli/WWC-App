d963d0ea4d98bbc01a15a3b9255e265a1181e64c
Yuceli <yuceli.polanco@gmail.com>
2015-02-01 16:32:04 -0600

    Se agregan funciones en UserController

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -17,12 +17,25 @@
 
 		public static function save($user)
 		{
-			
+	     
+	     $con = getConnection(); 
+	     $statment = $con->prepare("INSERT INTO users (name, lastname, nickname, password, email) VALUES (?, ?, ?, ?, ?)");
+         $statment->bind_param("sssss", $user->name, $user->lastname, $user->nickname, $user->password, $user->email);
+         $statment->execute();
+         $statment->close();
+         $con->close();
+
 		}
 
 		public static function update($user)
 		{
-			
+		 $con = getConnection(); 
+	     $statment = $con->prepare("UPDATE users SET name=?, lastname=?, nickname=?, password=?, email=? WHERE id=?");
+         $statment->bind_param("sssssi", $user->name, $user->lastname, $user->nickname, $user->password, $user->email, $user->id);
+         $statment->execute();
+         $statment->close();
+         $con->close();
+
 		}
 
 		public static function all()
@@ -32,7 +45,13 @@
 
 		public static function delete($id)	
 		{
-			
+		
+		 $con = getConnection(); 
+	     $statment = $con->prepare("DELETE FROM users WHERE id=?");
+         $statment->bind_param("i", $user->id);
+         $statment->execute();
+         $statment->close();
+         $con->close();	
 		}
 
 		public static function getById($id)
