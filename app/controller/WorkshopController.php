253017e3a153fea88fa34119be1ba7da0d1ee44d
Yuceli <yuceli.polanco@gmail.com>
2015-02-01 16:44:44 -0600

    Se agregan funciones en Workshops Controller

--- app/controller/WorkshopController.php
+++ app/controller/WorkshopController.php
@@ -16,12 +16,22 @@
 
 		public static function save($workshop)
 		{
-			
+			 $con = getConnection(); 
+		     $statment = $con->prepare("INSERT INTO workshops (title, description, begin_date, end_date) VALUES (?, ?, ?, ?)");
+	         $statment->bind_param("ssss", $workshop->title, $workshop->description, $workshop->begin_date, $workshop->end_date);
+	         $statment->execute();
+	         $statment->close();
+	         $con->close();
 		}
 
 		public static function update($workshop)
 		{
-			
+			 $con = getConnection(); 
+		     $statment = $con->prepare("UPDATE workshops SET title=?, description=?, begin_date=?, end_date=?, WHERE id=?");
+	         $statment->bind_param("ssssi", $workshop->name, $workshop->lastname, $workshop->nickname, $workshop->password, $workshop->email, $user->id);
+	         $statment->execute();
+	         $statment->close();
+	         $con->close();
 		}
 
 		public static function all()
@@ -31,7 +41,12 @@
 
 		public static function delete($id)	
 		{
-			
+			 $con = getConnection(); 
+		     $statment = $con->prepare("DELETE FROM workshops WHERE id=?");
+	         $statment->bind_param("i", $workshop->id);
+	         $statment->execute();
+	         $statment->close();
+	         $con->close();	
 		}
 
 		public static function getById($id)
