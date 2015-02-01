fac4891161816c291e2b52d56250df62dbfecc9c
shk33 <m.coronel.seg@gmail.com>
2015-02-01 16:34:26 -0600

    Add workshop signature methods

--- app/controller/WorkshopController.php
+++ app/controller/WorkshopController.php
@@ -13,8 +13,29 @@
 		{
 			
 		}
-	
 
+		public static function save($workshop)
+		{
+			
+		}
 
+		public static function update($workshop)
+		{
+			
+		}
 
+		public static function all()
+		{
+			
+		}
+
+		public static function delete($id)	
+		{
+			
+		}
+
+		public static function getById($id)
+		{
+			# code...
+		}
 	}
