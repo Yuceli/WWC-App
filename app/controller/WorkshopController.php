4025f46105494b21249d28a7f8d019d5e074dd08
Gilberto Leon <ripflame@gmail.com>
2015-02-03 18:36:33 -0600

    Agregando getById para cursos

--- app/controller/WorkshopController.php
+++ app/controller/WorkshopController.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 
 	/**
-	* 
+	*
 	*/
 
 	//namespace 'app\controller'
 
 	class WorkshopController
 	{
-		
+
 		function __construct()
 		{
-			
+
 		}
 
 		public static function save($workshop)
 		{
-			 $con = getConnection(); 
+			 $con = getConnection();
 		     $statment = $con->prepare("INSERT INTO workshops (title, description, begin_date, end_date) VALUES (?, ?, ?, ?)");
 	         $statment->bind_param("ssss", $workshop->title, $workshop->description, $workshop->begin_date, $workshop->end_date);
 	         $statment->execute();
@@ -26,7 +26,7 @@
 
 		public static function update($workshop)
 		{
-			 $con = getConnection(); 
+			 $con = getConnection();
 		     $statment = $con->prepare("UPDATE workshops SET title=?, description=?, begin_date=?, end_date=?, WHERE id=?");
 	         $statment->bind_param("ssssi", $workshop->name, $workshop->lastname, $workshop->nickname, $workshop->password, $workshop->email, $user->id);
 	         $statment->execute();
@@ -59,18 +59,31 @@
 			return $cursos;
 		}
 
-		public static function delete($id)	
+		public static function delete($id)
 		{
-			 $con = getConnection(); 
+			 $con = getConnection();
 		     $statment = $con->prepare("DELETE FROM workshops WHERE id=?");
-	         $statment->bind_param("i", $workshop->id);
+	         $statment->bind_param("i", $id);
 	         $statment->execute();
 	         $statment->close();
-	         $con->close();	
+	         $con->close();
 		}
 
 		public static function getById($id)
 		{
-			# code...
+			$con = getConnection();
+      $result = $con->query( " SELECT * FROM workshops WHERE id={$id}");
+      if ( $result->num_rows == 1 ) {
+        $row = $result->fetch_assoc();
+        $curso = new stdClass();
+        $curso->id          = $row["id"];
+        $curso->title       = $row["title"];
+        $curso->description = $row["description"];
+        $curso->begin_date  = $row["begin_date"];
+        $curso->end_date    = $row["end_date"];
+
+        return $user;
+      }
+      $con->close();
 		}
 	}
